85d11c3a85eb8c51bfcd8c6e08eb92e600948da2
Talarek87 <Squashypl@gmail.com>
2020-12-03 15:09:47 +0100

    1.0

--- Procfile
+++ Procfile
@@ -3,4 +3,4 @@ release: python apka/manage.py makemigrations --no-input
 release: python apka/manage.py migrate --no-input
 
 
-web: python apka/manage.py runserver 0.0.0.0:5000
+web: gunicorn apka.wsgi
--- apka/api_basic/models.py
+++ apka/api_basic/models.py
@@ -7,8 +7,8 @@ class Article(models.Model):
     title = models.CharField(max_length=1000)
     date = models.DateTimeField(auto_now_add=True)
     source = models.CharField(max_length=1000)
-    cont_art = models.EmailField(max_length=1000)
-    picture = models.EmailField(max_length=1000)
+    cont_art = models.CharField(max_length=1000)
+    picture = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.title
